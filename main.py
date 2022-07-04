c65b7433cad51472ed5cef304ecc7192c84beb5a
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-04 02:12:30 -0300

    python

---
+++ main.py
@@ -0,0 +1,11 @@
+n1 = int(input('Ingrese el valor: '))
+n2 = int(input('Ingrese el valor: '))
+
+resultado = n1 + n2
+
+print("el resultado es", resultado)
+
+if(resultado > 100):
+    print('es mayor que 100')
+else:
+    print('Es menor que 100')
