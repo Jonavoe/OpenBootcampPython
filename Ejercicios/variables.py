e1a3322df44e5813e85242f7d3f55d3fbfff0390
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-08 00:38:10 -0300

    Ejercicio variables

---
+++ Ejercicios/variables.py
@@ -0,0 +1,25 @@
+nombre = 'Jonathan'
+apellido = 'Voeffray'
+edad = 30
+email = 'voeffray.jonthan@gmail.com'
+telefono = 3487229333
+casado = True
+hijos = False
+amigos = ['Belen', 'Harrison', 'Negri']
+peliculas = {
+    'pelicula1': 'el amanecer de los muertos',
+    'pelicula2': 'esta bien no estar bien',
+    'pelicula3': 'propuesta laboral',
+    'pelicula4': 'el diario de noa',
+    
+}
+
+print(nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(amigos)
+print(peliculas)
