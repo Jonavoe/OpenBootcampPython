e07da59d579a2e93af88215b64b3c0f0bcb173ff
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-11 14:26:12 -0300

    Create trianguloCirculo.py

---
+++ Ejercicios/trianguloCirculo.py
@@ -0,0 +1,14 @@
+import math
+
+base = int(input('Ingrese la base del triangulo: '))
+altura = int(input('Ingrese la altura del triangulo: '))
+
+areaTriangulo = (base * altura) / 2
+
+print('El area del trinagulo es de', areaTriangulo)
+
+radio = int(input('Ingrese el radio del circulo: '))
+
+areaCirculo = (math.pi * radio**2)
+
+print('El area del trinagulo es de', areaCirculo)
