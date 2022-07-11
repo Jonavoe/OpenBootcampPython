de4df6d41ee879ebf673aecab3a8bf9e904659c7
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-11 10:37:12 -0300

    mayorDeEdad

---
+++ Ejercicios/mayorDeEdad.py
@@ -0,0 +1,10 @@
+print('-------------')
+print('Mayor de Edad')
+print('-------------')
+
+edad = int(input('Ingrese su Edad: '))
+
+if edad >= 18:
+    print('Eres mayor de edad!')
+else:
+    print('Eres menor de edad!')
