ff251d0836160dda6ee1cd6553df89f553d85884
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-12 06:55:48 -0300

    Create bisiesto.py

---
+++ Ejercicios/bisiesto.py
@@ -0,0 +1,18 @@
+def bisiesto (año):
+    if año % 4 == 0:
+        print(año, 'Es un Año bisiesto')
+    else:
+        print(año, 'Es un Año normal')
+        
+        
+
+bisiesto(1992)
+bisiesto(1993)
+bisiesto(1994)
+bisiesto(1995)
+bisiesto(1996)
+bisiesto(2022)
+bisiesto(2023)
+bisiesto(2024)
+bisiesto(2025)
+bisiesto(2026)
