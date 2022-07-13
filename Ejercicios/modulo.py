d539786135842682569a8b99c10ace7a7dd24444
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-07-13 13:45:04 -0300

    creo carpeta Operaciones y archivo modulo

---
+++ Ejercicios/modulo.py
@@ -0,0 +1,9 @@
+import Operaciones.sumar as s
+import Operaciones.restar as r
+import Operaciones.multiplicar as m
+import Operaciones.dividir as d
+
+print('El resultado de la Suma es:',s.sumar(7, 8))
+print('El resultado de la Resta es:',r.restar(10, 3))
+print('El resultado de la Multiplicacion es:',m.multiplicar(3, 2))
+print('El resultado de la Division es:',d.dividir(4, 2))
